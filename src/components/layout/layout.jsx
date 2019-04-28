ad2f506c9cc18f3f2b55a7e6d97a240af3bab9fc
Jillian Ogle <jill@letsrobot.tv>
2019-04-28 16:33:11 -0700

    cleanup

--- src/components/layout/layout.jsx
+++ src/components/layout/layout.jsx
@@ -4,59 +4,6 @@ import User from "./nav/user";
 import Chat from "./chat/chat";
 
 export default class Layout extends Component {
-  //Will slowly move all the event handling to the Event Handler.
-  // componentDidMount() {
-  //   this.handleResponse();
-  // }
-
-  // setUser = async user => {
-  //   //console.log("set user", user);
-  //   const { socket } = this.props;
-  //   await socket.emit(USER_CONNECTED, user);
-  //   this.setState({ user });
-  //   this.props.onEvent(LOGIN_TRUE, user);
-  // };
-
-  // handleClick = e => {
-  //   console.log("Handle Click");
-  // };
-
-  // //TODO: MOve these into the event handler
-  // handleResponse = () => {
-  //   const { socket } = this.props;
-  //   if (socket !== null) {
-  //     //If there is a socket, do stuff!
-  //     socket.on(USER_CONNECTED, user => {
-  //       if (user) {
-  //         this.setState({ user: user });
-  //         console.log("User Connected: ", user);
-  //       } else {
-  //         console.log("Unable to get User");
-  //       }
-  //     });
-  //     socket.on(USER_DISCONNECTED, disconnectUser => {
-  //       if (disconnectUser && disconnectUser["id"] === this.state.user["id"]) {
-  //         this.setState({ user: null });
-  //       } else {
-  //         console.log(
-  //           "Either someone else logged out, or there was an error with logging out"
-  //         );
-  //       }
-  //     });
-  //   }
-  // };
-
-  // handleLogout = () => {
-  //   const { user } = this.state;
-  //   const { socket } = this.props;
-  //   console.log("Handle Logout: ", user);
-  //   user !== null
-  //     ? socket.emit(LOGOUT, user, () => {
-  //         console.log("Logging Out");
-  //       })
-  //     : console.log("User Logout Error");
-  // };
-
   render() {
     const { socket, onEvent, chatroom, user, setUser } = this.props;
     return (

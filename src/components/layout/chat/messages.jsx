815c86df9b1cd58b02a63f653204f8ada8274c04
Jillian Ogle <jill@letsrobot.tv>
2019-04-30 17:26:26 -0700

    chat now scrolls! YAY!~

--- src/components/layout/chat/messages.jsx
+++ src/components/layout/chat/messages.jsx
@@ -2,6 +2,24 @@ import React, { Component } from "react";
 import Message from "./message";
 
 export default class Messages extends Component {
+  constructor(props) {
+    super(props);
+    this.scrollDown = this.scrollDown.bind(this);
+  }
+
+  scrollDown() {
+    const { container } = this.refs;
+    container.scrollTop = container.scrollHeight;
+  }
+
+  componentDidMount() {
+    this.scrollDown();
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    this.scrollDown();
+  }
+
   displayMessages = messages => {
     //console.log("messages", messages);
 
@@ -19,7 +37,9 @@ export default class Messages extends Component {
         <div className="chat-header">
           {" "}
           chat header
-          {this.displayMessages(this.props.messages)}
+          <div ref="container" className="chat-scroll">
+            {this.displayMessages(this.props.messages)}
+          </div>
         </div>
       </React.Fragment>
     );

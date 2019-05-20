d2af7f6690dbef0979b761c4595e64a61d6eacd0
Jillian Ogle <jill@letsrobot.tv>
2019-05-19 17:54:44 -0700

    added chat commands model, put in /me for example

--- src/models/chatMessage.js
+++ src/models/chatMessage.js
@@ -1,4 +1,5 @@
 const { makeId, createTimeStamp } = require("../modules/utilities");
+const { getMessageType } = require("./chatCommands");
 
 module.exports.createMessage = message => {
   const { saveMessageToActiveChat } = require("./chatRoom");
@@ -16,6 +17,7 @@ module.exports.createMessage = message => {
   //Turn this back on once you start removing active chats from memmory
   //saveMessageToActiveChat(makeMess);
 
+  getMessageType(makeMess);
   this.sendMessage(makeMess);
 };
 
@@ -25,55 +27,3 @@ module.exports.sendMessage = message => {
   console.log("Chat Room from SendMessage: ", chatRoom);
   io.to(chatRoom).emit("MESSAGE_RECIEVED", message);
 };
-
-module.exports.messageType = message => {
-  const command = RegExp("/");
-
-  //Check entry character & assign message types
-  if (message.message.charAt(0) === "/") message.type = "site-command";
-  if (message.message.charAt(0) === ".") message.type = "robot-command";
-  if (message.message.charAt(0) === "#") message.type = "donate";
-
-  //execute based on message types
-  if (message.type === "site-command") siteCommands(message);
-
-  //send final message to chat
-  return message;
-};
-
-//commands for managing the site through chat
-siteCommands = message => {
-  let updateCommand = message;
-  let scrubCommand = message.message
-    .substr(1)
-    .split(" ")[0]
-    .toLowerCase();
-
-  if (scrubCommand === "me") updateCommand = me(updateCommand);
-  if (scrubCommand === "w") message.type = "whisper";
-  if (scrubCommand === "timeout") message.type = "moderation";
-  if (scrubCommand === "mod") message.type = "moderation";
-  if (scrubCommand === "unmod") message.type = "moderation";
-
-  console.log("Do Command: ", scrubCommand);
-  message = updateCommand;
-  return message;
-};
-
-me = message => {
-  message.type = "self";
-  message.message = message.message.substr(4);
-  console.log(message.message);
-  return message;
-};
-
-//Moderate
-moderate = (moderate, validate) => {
-  //Does the command ( moderate ) come from a moderator ( validate )?
-  //If so, grab the name of the person to be moderated.
-  //execute moderation commands.
-};
-
-// commandList = {
-//    timeout: timeout,
-// }

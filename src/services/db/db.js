97ba5cc776e9bdad361ddad39b4555b5d612a64e
Jillian Ogle <jill@letsrobot.tv>
2019-05-19 17:23:07 -0700

    deprecating db.js

--- src/services/db/db.js
+++
@@ -1,7 +0,0 @@
-//old DB, may depricate
-
-const { Client } = require("pg");
-const client = new Client({ user: "postgres", database: "remote_control" });
-
-client.connect();
-module.exports = client;

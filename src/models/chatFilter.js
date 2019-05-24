d517709d9c9c218f506fb4201a917392ea7579b9
Bryan Morrison <bryan@Carbon.ourhouse.local>
2019-05-24 10:18:14 -0700

    better return function, remove console spam

--- src/models/chatFilter.js
+++ src/models/chatFilter.js
@@ -7,16 +7,14 @@ var metaphone = require("metaphone");
 const phoneticBadWords = globalBadWordsList.phonetic_bad_words;
 
 export const filterMessage = payload => {
-  console.log("phonetic bad words: ", phoneticBadWords)
+  let returnPayload = payload;
   const messageWords = payload.split(" ");
   messageWords.forEach(word => {
-    console.log("phonetic word: ", metaphone(word));
     if (phoneticBadWords.indexOf(metaphone(word)) > -1) {
-      console.log("phonetic filter match!");
-      return "/me said a bad word!";
+      returnPayload = "I said a bad word!";
     }
   });
-  return payload;
+  return returnPayload;
 };
 
-export default filterMessage;
+

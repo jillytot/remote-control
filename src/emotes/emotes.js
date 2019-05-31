0760cdb90b24a0c395d1ffde1ffb2eb03b164426
Bryan Morrison <skeeter_mcbee@live.com>
2019-05-31 11:40:29 -0700

    I want my face too

--- src/emotes/emotes.js
+++ src/emotes/emotes.js
@@ -2,12 +2,14 @@ import robotHi from "./imgs/robotHi.png";
 import robotHype from "./imgs/robotHype.png";
 import robotYay from "./imgs/robotYay.png";
 import jill from "./imgs/jill.png";
+import skeeter from "./imgs/skeeter.png";
 
 const emotes = {
   robotHi: robotHi,
   robotHype: robotHype,
   robotYay: robotYay,
-  jill: jill
+  jill: jill,
+  skeeter: skeeter
 };
 
 export default emotes;

63afd35e31f0667df1c4cc665aae32c8de2b4120
bmorrison4 <skeeter_mcbee@live.com>
2019-06-02 15:52:09 -0700

    update to replace the bad word with a substitute

--- src/models/chatFilter.js
+++ src/models/chatFilter.js
@@ -1,17 +1,22 @@
 const globalBadWordsList = require("./globalBadWordsList.json");
 const metaphone = require("metaphone");
 
-const phoneticBadWords = globalBadWordsList.phonetic_bad_words;
+const phoneticBadWords = Object.keys(globalBadWordsList.phonetic_bad_words);
 
 module.exports.filterMessage = payload => {
   let returnPayload = payload;
-  const messageWords = payload.split(" ");
-  messageWords.forEach(word => {
-    if (phoneticBadWords.indexOf(metaphone(word)) > -1) {
-      returnPayload = "I said a bad word!";
+  // split the payload at each space. Also works for single word payloads.
+  const words = payload.split(" ");
+  
+  for (let derp = 0; derp > words.length(); derp++) {
+    const foundWord = phoneticBadWords.indexOf(metaphone(words[derp]));
+    if (foundWord >= 0) {
+      returnPayload += globalBadWordsList.phonetic_bad_words[derp] + " ";
+    } else {
+      returnPayload += words[derp] + " ";
     }
-  });
-  return returnPayload;
+  }
+  return returnPayload.trim();
 };
 
 

775ac35fd11904b9194e560be5fca7f3daa317f0
bmorrison4 <skeeter_mcbee@live.com>
2019-06-06 16:40:43 -0700

    changed to appease ged et al

--- src/routes/api/signup.js
+++ src/routes/api/signup.js
@@ -5,18 +5,26 @@ const serverSettings = require("../../config/serverSettings");
 
 router.post("/", async (req, res) => {
   // post request
-  axios
-    .post(
-      `https://www.google.com/recaptcha/api/siteverify?secret=${serverSettings.reCaptchaSecretKey}&response=${req.body.response}`,
-      {}
-    )
-    .then(async resp => {
+  try {
+    const captcha = await axios.post(
+      `https://www.google.com/recaptcha/api/siteverify?secret=${
+        serverSettings.reCaptchaSecretKey
+      }&response=${req.body.response}`
+    );
+    delete req.body.response;
+    console.log(captcha.data);
+    if (captcha.data.success) {
+      console.log("got here");
       const createUser = await user.createUser(req.body);
+      console.log("and here");
       createUser !== null ? res.send(createUser) : res.send("ok");
-    })
-    .catch(err => {
-      console.error(`${err.response.status} at ${err.response.config.url}`);
-    });
+      console.log("here too");
+    } else {
+      console.error("Captcha failed!")
+    }
+  } catch (err) {
+    console.error(`${err.response.status} at ${err.response.config.url}`);
+  }
 });
 
 module.exports = router;

f444d92da5361625096aec5fb301ac0c58ebab6f
bmorrison4 <skeeter_mcbee@live.com>
2019-06-18 15:14:13 -0700

    add strict non phonic filter

--- src/models/chatFilter.js
+++ src/models/chatFilter.js
@@ -1,32 +1,57 @@
 const globalBadWordsList = require("./globalBadWordsList.json");
-const metaphone = require("metaphone");
-
-const phoneticBadWords = Object.keys(globalBadWordsList.phonetic_bad_words);
-const replacementWords = Object.values(globalBadWordsList.phonetic_bad_words);
 
 /**
  * Scan the message for offending phonetic matches and replace them with a
- * substitute.
+ * substitute. Ideal for TTS.
  *
  * @param {String} payload the message/username/any string to scan
  * @example
  *  // Filter a chat message
- *  chatMessage = filterMessage(chatMessage);
+ *  chatMessage = filterPhoneticMessage(chatMessage);
  *
  * @returns {String} the string but with any matching phonics replaced with family friendly fun time
  */
-module.exports.filterMessage = payload => {
+module.exports.filterPhoneticMessage = payload => {
+  const metaphone = require("metaphone");
+  const phoneticBadWords = Object.keys(globalBadWordsList.phonetic_bad_words);
+  const replacementWords = Object.values(globalBadWordsList.phonetic_bad_words);
   let returnPayload = "";
   // Split the payload at each space. Also works for single word payloads.
   const words = payload.split(" ");
   // iterate through the message for bad words
   words.forEach(word => {
     const idx = phoneticBadWords.indexOf(metaphone(word));
-    if (idx >= 0) { // substitute as needed
+    if (idx >= 0) {
+      // substitute as needed
       returnPayload += replacementWords[idx] + " ";
     } else {
       returnPayload += word + " ";
     }
   });
+  return returnPayload.trim();
+};
+
+module.exports.filterTextMessage = payload => {
+  let returnPayload = "";
+  /** @TODO split on any non-word character */
+  const messageSplit = payload.split(" ");
+  const badWords = Object.values(globalBadWordsList.normal_bad_words);
+  const replacements = Object.keys(globalBadWordsList.normal_bad_words);
+  messageSplit.forEach((word) => {
+    let index = -1;
+    let triggered = false;
+    badWords.forEach((words, idx) => {
+      if (words.indexOf(word) >= 0) {
+        triggered = true;
+        index = idx;
+      }
+    })
+    if (triggered) {
+      returnPayload += replacements[index] + " ";
+    } else {
+      returnPayload += word + " ";
+    }
+  });
+
   return returnPayload.trim();
 };

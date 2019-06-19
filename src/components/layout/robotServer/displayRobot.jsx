5d7d563aacd918422ae08941a57efed6576502e2
Jillian Ogle <jill@letsrobot.tv>
2019-06-18 19:05:36 -0700

    Add Robot

--- src/components/layout/robotServer/displayRobot.jsx
+++ src/components/layout/robotServer/displayRobot.jsx
@@ -5,7 +5,7 @@ export default class DisplayRobot extends Component {
   state = {};
 
   handleDisplay = () => {
-    const { server, user } = this.props;
+    const { server, user, channels } = this.props;
     console.log(this.props);
     if (server && user && user.id === server.owner_id) {
       return (
@@ -14,6 +14,13 @@ export default class DisplayRobot extends Component {
           <div className="display-robot-item"> Robot 1</div>
           <div className="display-robot-item"> Robot 2</div>
           <div className="display-robot-item"> Robot 3</div>
+          <AddRobot
+            server={server}
+            user={user}
+            channels={channels}
+            modal={this.props.modal}
+            onCloseModal={this.props.onCloseModal}
+          />
         </div>
       );
     }
@@ -21,5 +28,39 @@ export default class DisplayRobot extends Component {
 
   render() {
     return <div>{this.handleDisplay()}</div>;
+  }
+}
+
+class AddRobot extends Component {
+  handleModal = () => {
+    return [
+      {
+        body: "Add Robot"
+      },
+      { header: "" },
+      { footer: "" }
+    ];
+  };
+
+  handleDisplayAddRobot() {
+    const { server, user, channels } = this.props;
+    if (channels && channels.length > 0 && user.id === server.owner_id) {
+      return (
+        <div
+          className="add-channel"
+          onClick={() => {
+            this.props.modal(this.handleModal());
+          }}
+        >
+          {" "}
+          + Add Robot ...
+        </div>
+      );
+    }
+    return <React.Fragment />;
+  }
+
+  render() {
+    return <React.Fragment>{this.handleDisplayAddRobot()}</React.Fragment>;
   }
 }

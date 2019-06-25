5302c0279f09840577bdf7eb9a84298436a5ba28
Jillian Ogle <jill@letsrobot.tv>
2019-06-25 13:26:35 -0700

    improving auth middleware, fixing typos

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,23 +1,28 @@
 const { authUser } = require("../models/user");
 
 const auth = async (req, res, next) => {
-  const header = req.headers["authorization"];
-  console.log("API AUTH: ", header);
+  try {
+    const header = req.headers["authorization"];
+    console.log("API AUTH: ", header);
 
-  if (typeof header !== "undefined") {
-    const bearer = header.split(" ");
-    const token = bearer[1];
-    req.token = token;
+    if (typeof header !== "undefined") {
+      const bearer = header.split(" ");
+      const token = bearer[1];
+      req.token = token;
 
-    req.user = await authUser(token);
-    if (!req.user) {
-      return res.json({ error: "Invalid Authorization" });
+      req.user = await authUser(token);
+      if (!req.user) {
+        return res.json({ error: "Invalid Authorization" });
+      } else {
+        next();
+      }
     } else {
-      next();
+      //If header is undefined return Forbidden (403)
+      res.sendStatus(403);
     }
-  } else {
-    //If header is undefined return Forbidden (403)
-    res.sendStatus(403);
+  } catch (e) {
+    console.error("Auth Middleware Error: ", e);
+    res.status(500).json({ error: "Internal Server Error" });
   }
 };
 

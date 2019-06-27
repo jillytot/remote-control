ba9e595ab7a28d263f7ab6aba2e1c8777cdd71d9
Jillian Ogle <jill@letsrobot.tv>
2019-06-27 16:00:05 -0700

    updated max timeout settings

--- src/config/serverSettings.js
+++ src/config/serverSettings.js
@@ -3,5 +3,5 @@ module.exports = {
   heartBeat: 10000,
   reCaptchaSecretKey: "6Lfg_KYUAAAAAILikAGmfmaR3IvYw3eeucDBp-TU",
   secret: "temp_secret",
-  maxTimeout: 15768000000 //6 months
+  maxTimeout: 15768000 //6 months
 };

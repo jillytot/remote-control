34be9abf963ae22d4d645e0b0bbe73b257c8eca4
Nocturnal42 <steele.peter@gmail.com>
2019-06-30 04:50:17 +1000

    Error fix
    Prevents error being thrown when message sent before robot has authenticated.

--- src/events/robotMessageSent.js
+++ src/events/robotMessageSent.js
@@ -1,10 +1,13 @@
 const { createMessage } = require("../models/chatMessage");
 
 module.exports = async (ws, message) => {
-  console.log("Robot Message Received: ", message);
-
-  const { getUserInfoFromId, publicUser } = require("../models/user");
-  message.user = { "username": ws.robot.name, "id": ws.robot.id };
-  message.type = "robot";
-  createMessage(message);
+  if (ws.robot) {
+    console.log("Robot Message Received: ", message);
+    const { getUserInfoFromId, publicUser } = require("../models/user");
+    message.user = { "username": ws.robot.name, "id": ws.robot.id };
+    message.type = "robot";
+    createMessage(message);
+  } else {
+    console.log("Robot Message Rejected (not authed): ", message);
+  }
 };

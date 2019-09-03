19ecbb94e8d50360036e9b48a101d4e59a192308
Jillian Ogle <jill@letsrobot.tv>
2019-09-03 13:36:33 -0700

    Reorganizing a bit

---
+++ src/supportingApps/index.js
@@ -0,0 +1,6 @@
+/*
+Supporting Apps are indepedent applications made to support remo. 
+Each file or folder should be it's own thing, 
+and will probably be eventually migrated to it's own repo down the line. 
+
+*/

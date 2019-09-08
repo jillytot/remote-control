08e987431cb5f5f10519e7369a5efd768ab3fa7f
Jillian Ogle <jill@letsrobot.tv>
2019-09-08 16:01:05 -0700

    adding header check in auth

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -6,21 +6,23 @@ const auth = options => {
   // console.log(options);
   return async (req, res, next) => {
     try {
-      //parse token as a first step
-      const bearer = req.headers["authorization"].split(" ");
-      const token = bearer[1];
-      const tokenData = await extractToken(token);
-      // console.log("////////////CHECK TOKEN DATA///////////", tokenData);
-      if (tokenData && tokenData.id) {
-        let type = tokenData.id.substring(0, 4);
+      if (req.headers.authorization) {
+        //parse token as a first step
+        const bearer = req.headers["authorization"].split(" ");
+        const token = bearer[1];
+        const tokenData = await extractToken(token);
+        // console.log("////////////CHECK TOKEN DATA///////////", tokenData);
+        if (tokenData && tokenData.id) {
+          let type = tokenData.id.substring(0, 4);
 
-        // console.log("API AUTH: ", tokenData);
-        if (type === "user" && options.user) {
-          //what
-          req.user = await authUserData(tokenData);
-        } else if (type === "rbot" && options.robot) {
-          //e you need t
-          req.robot = await authRobotData(tokenData);
+          // console.log("API AUTH: ", tokenData);
+          if (type === "user" && options.user) {
+            //what
+            req.user = await authUserData(tokenData);
+          } else if (type === "rbot" && options.robot) {
+            //e you need t
+            req.robot = await authRobotData(tokenData);
+          }
         }
       } else {
         return res.json({ error: "Invalid Authorization" });

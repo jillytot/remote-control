34b554c7639e75f0c27b1e2ed246451457deee15
Jillian Ogle <jill@letsrobot.tv>
2019-11-01 14:39:00 -0700

    another rollback

--- src/scripts/removeDupeControls.js
+++ src/scripts/removeDupeControls.js
@@ -2,48 +2,20 @@
 //If found, remove older entries, and only keep the newest one.
 
 const removeControlDupes = async () => {
-  const { getAllControls, removeControls } = require("../models/controls");
-  const { setControls } = require("../models/channel");
+  const { getAllControls } = require("../models/controls");
   const listControls = await getAllControls();
-  console.log("Entries Found: ", listControls.length);
-  const promises = listControls.map(dupeCheck => {
-    return async () => {
-      let getDupes = listControls.filter(
-        listItem => listItem.channel_id === dupeCheck.channel_id
-      );
-      if (getDupes[1]) {
-        console.log("DUPES FOUND!: ", getDupes.length);
-        await getDupes.sort(compare);
-        await getDupes.forEach(async (dupe, index) => {
-          console.log(dupe.created, index);
-          if (index === 0) {
-            //set channel controls to this control
-          } else {
-            const result = await removeControls(dupe);
-            console.log(result);
-          }
-        });
-      }
-    };
+  //   console.log(listControls);
+  await listControls.map(dupeCheck => {
+    const getDupes = listControls.filter(
+      listItem => listItem.channel_id === dupeCheck.channel_id
+    );
+    if (getDupes[1]) {
+      console.log("DUPES FOUND!: ");
+      getDupes.map(dupe => {
+        console.log(dupe.id, dupe.channel_id, dupe.created);
+      });
+    }
   });
-
-  await Promise.all(promises);
-  process.exit(1);
-};
-
-const enforceDefaultControls = async () => {};
-
-const compare = async (a, b) => {
-  const serverA = a.created;
-  const serverB = b.created;
-
-  let comparison = 0;
-  if (serverA > serverB) {
-    comparison = 1;
-  } else if (serverA < serverB) {
-    comparison = -1;
-  }
-  return comparison * -1;
 };
 
 removeControlDupes();

9ab1ba914f6fbc9b7cc5dadc77a74c1f0ab67e23
Jillian Ogle <jill@letsrobot.tv>
2019-12-19 13:32:54 -0800

    Fixing signup logic

--- src/routes/api/signup.js
+++ src/routes/api/signup.js
@@ -19,22 +19,26 @@ router.post("/", async (req, res) => {
       //Validate
       let data = req.body;
       data.username = validateUserName(data.username);
-      if (data.username.error) res.send(data.username);
+      if (data.username.error) return res.send(data.username);
       data.email = validateUserEmail(data.email);
-      if (data.email.error) res.send(data.email);
+      if (data.email.error) return res.send(data.email);
 
       //Create User
       const createUser = await user.createUser(data);
       createUser !== null ? res.send(createUser) : res.send("ok");
+      return;
     } else {
       console.error("Captcha failed!");
       res.send(
         jsonError("Unable to validate capture, please reload and try again.")
       );
+      return;
     }
   } catch (err) {
-    res.send(jsonError(`${err.response.status} at ${err.response.config.url}`));
+    // res.send(jsonError(`${err.response.status} at ${err.response.config.url}`));
     // console.error(`${err.response.status} at ${err.response.config.url}`);
+    console.log(err);
+    res.send(jsonError("There wasa problem creating an account."));
   }
 });
 

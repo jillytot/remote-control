4f60daa7bedea3fbce92d4489265d71dfd577641
Jillian Ogle <jill@letsrobot.tv>
2020-02-03 15:14:42 -0800

    Update Channel Name model

--- src/models/channel.js
+++ src/models/channel.js
@@ -221,6 +221,18 @@ module.exports.getServerIdFromChannelId = async channel_id => {
   }
 };
 
+module.exports.renameChannel = async ({ name, id }) => {
+  const db = require("../services/db");
+  const query = `UPDATE channels SET name = $1 WHERE id = $2 RETURNING *`;
+  try {
+    const result = await db.query(query, [name, id]);
+    if (result.rows[0]) return result.rows[0];
+  } catch (err) {
+    console.log(err);
+  }
+  return jsonError("Unable to name channel, please try again later");
+};
+
 module.exports.setControls = async controlData => {
   const { sendUpdatedControls } = require("./controls");
   // console.log("SET CONTROLS CHECK: ", controlData);

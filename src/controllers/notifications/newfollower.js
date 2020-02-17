9df21ff965334259f71c9ad91f839afacbd2c13b
Jillian Ogle <jill@letsrobot.tv>
2020-02-17 14:52:03 -0800

    Settings check on join server messages.

--- src/controllers/notifications/newfollower.js
+++ src/controllers/notifications/newfollower.js
@@ -1,16 +1,21 @@
-module.exports = (server, follower) => {
+module.exports = async (server, follower) => {
   const { createMessage } = require("../../models/chatMessage");
   const { alertMessages } = require("./");
+  const { getRobotServerSettings } = require("../../models/robotServer");
   //Todo: Check server settings for printing new follower to chat
-  //Todo: Check user settings for emailing follower alerts
+  const { settings } = await getRobotServerSettings(server.server_id);
+  console.log("Get Settings for Server: ", settings);
+  if (settings.announce_followers_in_chat !== false) {
+    const { username } = follower;
+    const alert = alertMessages(username);
+    createMessage({
+      message: alert,
+      user: follower,
+      server_id: server.server_id,
+      type: "event",
+      broadcast: "server"
+    });
+  }
 
-  const { username } = follower;
-  const alert = alertMessages(username);
-  createMessage({
-    message: alert,
-    user: follower,
-    server_id: server.server_id,
-    type: "event",
-    broadcast: "server"
-  });
+  //Todo: Check user settings for emailing follower alerts
 };

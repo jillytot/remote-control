1fe33defe25956d2e13a99c81ee490b5c14e3a7f
Jillian Ogle <jill@letsrobot.tv>
2020-02-23 14:00:33 -0800

    New Alert Message

--- src/controllers/notifications/alertMessages.js
+++ src/controllers/notifications/alertMessages.js
@@ -7,7 +7,8 @@ module.exports = username => {
     `${username} detected, raising shields.`,
     `${username} is now one of us! One of us!`,
     `${username} just joined, 10/10 would join again.`,
-    `Welcome ${username}! robotYay robotHi`
+    `Welcome ${username}! robotYay robotHi`,
+    `Program "${username}" has been installed successfully!`
   ];
   const randomIndex = Math.floor(Math.random() * Math.floor(alerts.length));
   return alerts[randomIndex];
